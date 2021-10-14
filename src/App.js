94f19d8fca8d79c389f36c26df2ab55cc7373e6d
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-10-14 12:48:37 -0400

    Added variable for YouTube API key.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { youtubeAPIKey } from './key'
+
 
 function App() {
   return (
+    
     <div className="App">
       <h1>Hello World</h1>
+      {console.log(youtubeAPIKey)}
     </div>
   );
 }
