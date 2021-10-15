1af313ac4f892cad83389c9f0434bdd570c0a3ad
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-10-15 15:00:39 -0400

    Passed in new props.

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -19,7 +19,7 @@ const VideoPlayer = (props) => {
                     <p>{props.videoInfo.description}</p>
                 <hr />
             </div>
-            <CommentSection videoId={props.videoId} />
+            <CommentSection videoId={props.videoId} videoComments={props.videoComments} />
             </div>
         </div>
         
