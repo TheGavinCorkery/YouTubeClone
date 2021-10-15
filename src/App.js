817b4f67bc071dc0115c76692544cf738aa30139
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-10-15 10:27:49 -0400

    Update App.js
    Added URLs and updated getSearchResults with params.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import SearchResultsList from './components/SearchResultsList/SearchResultsList'
 
 
 // URL = `https://www.googleapis.com/youtube/v3/search?q=${searchQuery}&key=${youtubeAPIKey}&type=video&maxResults=3`
-URL = 'https://www.googleapis.com/youtube/v3/'
 
 class App  extends Component {
   constructor(props) {
@@ -20,17 +19,27 @@ class App  extends Component {
      }
   }
 
+  baseURL = 'https://www.googleapis.com/youtube/v3/'
+  searchURL = 'https://www.googleapis.com/youtube/v3/search'
+
   // latestSearchResults =  null
 
   getSearchResults = async (query) => {
-    const params = {
-      "q": query,
-      "key": youtubeAPIKey,
-      "type": "video",
-      "maxResults": 3
-    }
+    // const params = {
+    //   "q": query,
+    //   "key": youtubeAPIKey,
+    //   "type": "video",
+    //   "maxResults": 3
+    // }
     // let response = await axios.get(`${this.URL}search?${params}`)
-    let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=React&key=${youtubeAPIKey}&type=video&maxResults=4`)
+    const response = await axios.get(this.searchURL, {
+      params: {
+        q: query,
+        key: youtubeAPIKey,
+        type: "video",
+        maxResults: 3
+      }
+    })
     console.log(response.data.items)
     console.log(response.data.items[0].id.videoId)
     // this.latestSearchResults = [response.data]
