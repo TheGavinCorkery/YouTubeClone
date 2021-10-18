a1fb1fe95beb29fdea12e1a8051f3e96937e6f4a
N. Hawkins <70733655+nhawks@users.noreply.github.com>
2021-10-18 15:17:44 -0400

    Imported custom theme.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './scss/custom.css'
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
