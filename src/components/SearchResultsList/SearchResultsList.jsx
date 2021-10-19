0a0609592051e1c473fc4ebfc65c5b4fc0bcd9f6
Gavin Corkery <GavinCorkery@hotmail.com>
2021-10-19 10:21:57 -0400

    Restyled the suggested video tab

--- src/components/SearchResultsList/SearchResultsList.jsx
+++ src/components/SearchResultsList/SearchResultsList.jsx
@@ -7,9 +7,9 @@ function SearchResultsList(props) {
     return ( 
         <div className="p-4">
             {props.results.map((video) => {
-                return <Card style= {{width: '15rem'}}>
-                            <a onClick = {() => props.playVideo(video)}><Card.Img variant = "top" src={video.snippet.thumbnails.high.url} alt="thumbnail"/></a>
-                            <Card.Text><p>{video.snippet.title}</p></Card.Text>
+                return <Card className = "card" style= {{width: "12rem"}}>
+                            <a onClick = {() => props.playVideo(video)}><Card.Img className = "img" src={video.snippet.thumbnails.high.url} alt="thumbnail"/></a>
+                            <Card.Text className = "text">{video.snippet.title}</Card.Text>
                         </Card>
             })}
         </div>
